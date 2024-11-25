5593e754b3ab96516e487e55461655ae64130a9f
banlang222 <master39@126.com>
2024-11-25 20:15:00 +0800

    修正num.decimal在isNaN时的容错处理

--- lib/src/num.dart
+++ lib/src/num.dart
@@ -6,20 +6,20 @@ extension NumExtension on num? {
   }
 
   String? get toTwoDigits {
-    if(isNull) return null;
+    if (isNull) return null;
     if (this! >= 10) return '${this}';
     return '0${this}';
   }
 
   String? get toThreeDigits {
-    if(isNull) return null;
-    if(this! >= 100) return '${this}';
-    if(this! >= 10) return '0${this}';
+    if (isNull) return null;
+    if (this! >= 100) return '${this}';
+    if (this! >= 10) return '0${this}';
     return '00${this}';
   }
 
   String? get toFourDigits {
-    if(isNull) return null;
+    if (isNull) return null;
     var absN = this!.abs();
     var sign = this! < 0 ? '-' : '';
     if (absN >= 1000) return '$this';
@@ -30,18 +30,25 @@ extension NumExtension on num? {
 
   //保留小数点后n位，int原样返回，小数点后为0则取整
   num? decimal(int n) {
-    if(this == null) return null;
-    if(this is double){
+    if (this == null) return null;
+    if (this is double &&
+        !this!.isNaN &&
+        !this!.isInfinite &&
+        !this!.isNegative) {
       var result = (this! * pow(10, n)).round() / pow(10, n);
       return result.round() == result ? result.round() : result;
-    } else if(this is int) {
+    } else if (this is int) {
       return this;
     }
     return null;
   }
 
   num? get tryToInt {
-    return isNull ? this : this == this!.toInt() ? this!.toInt() : this;
+    return isNull
+        ? this
+        : this == this!.toInt()
+            ? this!.toInt()
+            : this;
   }
 
   String? get tryToIntString {
