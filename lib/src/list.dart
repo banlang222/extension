68b14605e441c5b4742e4c77e34d48b76e3c17e4
banlang222 <master39@126.com>
2024-12-10 18:07:09 +0800

    list map equalTo

--- lib/src/list.dart
+++ lib/src/list.dart
@@ -1,22 +1,22 @@
 import 'dart:math' as math;
+
 import 'map.dart';
+
 extension ListExtension<E> on List<E>? {
   bool get isNullOrEmpty {
     return this == null ? true : this!.isEmpty;
   }
 
   List<E>? copy() {
-    if(this == null) return null;
-    if(this!.isEmpty) return this;
+    if (this == null) return null;
+    if (this!.isEmpty) return this;
     var _list = <E>[];
     this!.forEach((element) {
-      if(element is Map){
+      if (element is Map) {
         _list.add(element.copy() as E);
-      }
-      else if(element is List){
+      } else if (element is List) {
         _list.add(element.copy() as E);
-      }
-      else {
+      } else {
         _list.add(element);
       }
     });
@@ -24,27 +24,60 @@ extension ListExtension<E> on List<E>? {
   }
 
   List<E>? getRandomList(int n) {
-    if(isNullOrEmpty) return this;
+    if (isNullOrEmpty) return this;
     var _list = <E>[];
     var _n = this!.length > n ? n : this!.length;
     var random = math.Random();
-    while(_list.length < n && _list.length < this!.length) {
+    while (_list.length < n && _list.length < this!.length) {
       var element = this!.elementAt(random.nextInt(_n));
-      if(!_list.contains(element)){
+      if (!_list.contains(element)) {
         _list.add(element);
       }
     }
     return _list;
   }
+
+  bool equalTo(List<E>? other) {
+    if (runtimeType != other.runtimeType) return false;
+    if (this == null || other == null) return false;
+    if (this!.length != other.length) return false;
+    bool equal = true;
+    for (int i = 0; i < this!.length; i++) {
+      if (this!.elementAt(i) is List &&
+          other.elementAt(i).runtimeType == this!.elementAt(i).runtimeType) {
+        if (!(this!.elementAt(i) as List).equalTo(other.elementAt(i) as List)) {
+          equal = false;
+          break;
+        }
+      } else if (this!.elementAt(i) is Map &&
+          other.elementAt(i).runtimeType == this!.elementAt(i).runtimeType) {
+        if (!(this!.elementAt(i) as Map).equalTo(other.elementAt(i) as Map)) {
+          equal = false;
+          break;
+        }
+      } else if (this!.elementAt(i).runtimeType !=
+          other.elementAt(i).runtimeType) {
+        equal = false;
+        break;
+      } else {
+        if (this!.elementAt(i) != other.elementAt(i)) {
+          equal = false;
+          break;
+        }
+      }
+    }
+    return equal;
+  }
 }
+
 extension SetExtension on Set? {
   bool get isNullOrEmpty {
     return this == null ? true : this!.isEmpty;
   }
 }
+
 extension IterableExtension on Iterable? {
   bool get isNullOrEmpty {
     return this == null ? true : this!.isEmpty;
   }
 }
-
--- lib/src/map.dart
+++ lib/src/map.dart
@@ -1,17 +1,18 @@
 import 'list.dart';
+
 extension MapExtension<K, V> on Map<K, V>? {
   bool get isNullOrEmpty {
     return this == null ? true : this!.isEmpty;
   }
 
   Map<K, V>? copy() {
-    if(this == null) return null;
-    if(this!.isEmpty) return this!;
+    if (this == null) return null;
+    if (this!.isEmpty) return this!;
     var _map = <K, V>{};
     this!.forEach((key, value) {
-      if(value is Map){
+      if (value is Map) {
         _map[key] = value.copy() as V;
-      } else if(value is List){
+      } else if (value is List) {
         _map[key] = value.copy() as V;
       } else {
         _map[key] = value;
@@ -21,21 +22,42 @@ extension MapExtension<K, V> on Map<K, V>? {
   }
 
   bool equalTo(Map<K, V>? other) {
-    if(runtimeType != other.runtimeType) return false;
-    if(this == null || other == null) return false;
-    if(this!.length != other.length) return false;
+    if (runtimeType != other.runtimeType) return false;
+    if (this == null || other == null) return false;
+    if (this!.length != other.length) return false;
     var equal = true;
-    for(var key in this!.keys){
-      if(!other.containsKey(key)){
-        equal = false;
-        break;
-      } else if(this![key] != other[key]){
+    for (var entry in this!.entries) {
+      //对比键
+      if (!other.containsKey(entry.key)) {
         equal = false;
         break;
       }
+      //对比值
+      else {
+        if (entry.value is Map &&
+            other[entry.key].runtimeType == entry.value.runtimeType) {
+          if (!(entry.value as Map).equalTo(other[entry.key] as Map)) {
+            equal = false;
+            break;
+          }
+        } else if (entry.value is List &&
+            other[entry.key].runtimeType == entry.value.runtimeType) {
+          if (!(entry.value as List).equalTo(other[entry.key] as List)) {
+            equal = false;
+            break;
+          }
+        } else if (entry.value.runtimeType != other[entry.key].runtimeType) {
+          equal = false;
+          break;
+        } else {
+          if (entry.value != other[entry.key]) {
+            equal = false;
+            break;
+          }
+        }
+      }
     }
+
     return equal;
   }
 }
-
-
